2e04d7a0eefa77b044ae96e39402cda823a5f1da
เจษฎา เทียนทอง <674244112@mail.pbru.ac.th>
2025-08-14 11:41:29 +0700

    Add files via upload

---
+++ child1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main(){
+	printf("My bio.: \n");
+	printf("ID : 674244112 \n");
+	   printf("Name : chesada thianthong\n");
+    printf("------------------------------\n");
+    getchar();
+}
